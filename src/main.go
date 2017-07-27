2252858c40476913af748cb6f32929c33a53408e
alvesmarcos <alves.henrique.marcos@gmail.com>
2017-07-27 00:01:26 -0300

    Adiciona mockup GetNameUser

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
@@ -9,11 +10,27 @@ import(
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
+	api := router.PathPrefix("/api/v1").Subrouter()
+	
+	// handlers test
+	api.HandleFunc("/", Index)
+	api.HandleFunc("/users", GetNameUser)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+// example function handle
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome!")
+}
+
+// get users mockup
+func GetNameUser(w http.ResponseWriter, r *http.Request) {
+	var users [2]User
+	users[0] = User{ Name: "Marcos", Password: "08quinho" }
+	users[1] = User{ Name: "Rubenita", Password: "mainha" }
+
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		panic(err)
+	}
 }
--- src/models.go
+++ src/models.go
@@ -1,22 +1,24 @@
+package main
+
 type User struct {
-	Name			string	`bson:"name", json:"name"`
-	Password 	string	`bson:"password", json:"password`
+	Name			string	`bson:"name" json:"name"`
+	Password 	string	`bson:"password" json:"password"`
 }
 
 type Report struct {
-	Title		string		`bson:"title", json:"title"`
-	Body 		string		`bson:"body", json:"body"`
-	Images	[]string	`bson:"images", json:"images"`
+	Title		string		`bson:"title" json:"title"`
+	Body 		string		`bson:"body" json:"body"`
+	Images	[]string	`bson:"images" json:"images"`
 }
 
 type Indicator struct {
-	Name				string	`bson:"name", json:"name"`
-	Description	string	`bson:"description", json:"description"`
-	Metric			string 	`bson:"", json:""`
-	Status			string 	`bson:"status", json:"status"`
-	Date 				string 	`bson:"date", json:"date"`
+	Name				string	`bson:"name" json:"name"`
+	Description	string	`bson:"description" json:"description"`
+	Metric			string 	`bson:"metric" json:"metric"`
+	Status			string 	`bson:"status" json:"status"`
+	Date 				string 	`bson:"date" json:"date"`
 	Samples			[] struct {
-		Date	string	`bson:"date", json:"date"`
-		Value	string	`bson:"value", json:"value"`
-	} `bson:"samples", json:"samples"`
+		Date	string	`bson:"date" json:"date"`
+		Value	string	`bson:"value" json:"value"`
+	} `bson:"samples" json:"samples"`
 }
