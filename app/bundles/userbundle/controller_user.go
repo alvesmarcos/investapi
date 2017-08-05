128292dfcd14341f35f674ab1cf5494be9217ba0
alvesmarcos <alves.henrique.marcos@gmail.com>
2017-08-05 02:42:32 -0300

    Adiciona msgs nas requisicoes

--- app/bundles/userbundle/controller_user.go
+++ app/bundles/userbundle/controller_user.go
@@ -1,20 +1,12 @@
 package userbundle
 
 import(
-  "errors"
-  "log"
   "io/ioutil"
   "net/url"
   "net/http"
 	"strconv"
 	"github.com/gorilla/mux"
   "github.com/alvesmarcos/investapi/app/core"
-)
-
-var (
-  ErrorNotFound         = errors.New("Not Found")
-  ErrorInvalidUser      = errors.New("Invalid username or password")
-  ErrorInvalidId        = errors.New("Invalid id")
 )
 
 type UserController struct {
@@ -33,7 +25,7 @@ func (c *UserController) Index(w http.ResponseWriter, r *http.Request) {
   users, err := c.ump.FindAll()
 
   if c.HandleError(err, w) {
-    c.SendJSON(w, ErrorNotFound, http.StatusNotFound)
+    c.SendJSON(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
     return
   }
   c.SendJSON(w, &users, http.StatusOK)
@@ -43,7 +35,7 @@ func (c *UserController) Create(w http.ResponseWriter, r *http.Request) {
   body, err := ioutil.ReadAll(r.Body)
 
   if err != nil {
-    c.SendJSON(w, nil, http.StatusInternalServerError)
+    c.SendJSON(w,  http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
     return
   }
 
@@ -52,12 +44,12 @@ func (c *UserController) Create(w http.ResponseWriter, r *http.Request) {
   user := User { Username: values.Get("username"), Password: values.Get("password") }
 
   if !user.Validate() {
-    c.SendJSON(w, ErrorInvalidUser, http.StatusBadRequest)
+    c.SendJSON(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
     return
   }
 
   if err = c.ump.Insert(&user); err != nil {
-    c.SendJSON(w, nil, http.StatusInternalServerError)
+    c.SendJSON(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
     return
   }
   c.SendJSON(w, user, http.StatusOK)
@@ -72,13 +64,13 @@ func (c *UserController) Get(w http.ResponseWriter, r *http.Request) {
   user := User { Username: username, Password: password }
 
   if !user.Validate() {
-    c.SendJSON(w, ErrorInvalidUser, http.StatusBadRequest)
+    c.SendJSON(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
     return
   }
 
   users, err := c.ump.FindUser(&user)
   if err != nil {
-    c.SendJSON(w, ErrorInvalidUser, http.StatusInternalServerError)
+    c.SendJSON(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
   }
 
   c.SendJSON(w, &users, http.StatusOK)
@@ -90,15 +82,15 @@ func (c *UserController) Delete(w http.ResponseWriter, r *http.Request) {
   id, err := strconv.Atoi(vars["id"])
 
   if c.HandleError(err, w) {
-    c.SendJSON(w, ErrorInvalidId, http.StatusBadRequest)
+    c.SendJSON(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
     return
   }
 
   if c.HandleError(c.ump.Delete(id), w) {
-    c.SendJSON(w, ErrorNotFound, http.StatusNotFound)
+    c.SendJSON(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
     return
   }
-  c.SendJSON(w, nil, http.StatusOK)
+  c.SendJSON(w, http.StatusText(http.StatusOK), http.StatusOK)
 }
 
 func (c *UserController) Update(w http.ResponseWriter, r *http.Request) {
@@ -106,7 +98,7 @@ func (c *UserController) Update(w http.ResponseWriter, r *http.Request) {
   body, err := ioutil.ReadAll(r.Body)
 
   if err != nil {
-    c.SendJSON(w, nil, http.StatusInternalServerError)
+    c.SendJSON(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
     return
   }
 
@@ -115,14 +107,14 @@ func (c *UserController) Update(w http.ResponseWriter, r *http.Request) {
   id, err := strconv.Atoi(vars["id"])
 
   if c.HandleError(err, w) {
-    c.SendJSON(w, ErrorInvalidId, http.StatusBadRequest)
+    c.SendJSON(w,  http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
     return
   }
   user := User { Id: id, Username: values.Get("username"), Password: values.Get("password") }
-    log.Println(user)
+
   if c.HandleError(c.ump.Update(&user), w) {
-    c.SendJSON(w, ErrorNotFound, http.StatusNotFound)
+    c.SendJSON(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
     return
   }
-  c.SendJSON(w, nil, http.StatusOK)
+  c.SendJSON(w, user, http.StatusOK)
 }
