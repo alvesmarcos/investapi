7360add9ffb67681e9c11414082165a7c1eb9a78
alvesmarcos <alves.henrique.marcos@gmail.com>
2017-08-09 22:39:03 -0300

    Adiciona mapper_psql_indicator

---
+++ app/bundles/indicatorbundle/mapper_psql_indicator.go
@@ -0,0 +1,46 @@
+package indicatorbundle
+
+import "github.com/jinzhu/gorm"
+
+type IndicatorMapperPSQL struct {
+  db  *gorm.DB
+}
+
+func NewIndicatorMapperPSQL(db *gorm.DB) *IndicatorMapperPSQL {
+  return &IndicatorMapperPSQL{ db: db }
+}
+
+func (imp *IndicatorMapperPSQL) FindAll() ([]Indicator, error) {
+  var indicators []Indicator
+
+  err := imp.db.Find(&indicators).Error
+
+  return indicators, err
+}
+
+func (imp *IndicatorMapperPSQL) FindReportById(id int) (Indicator, error) {
+  var indicator Indicator
+
+  err := imp.db.Where("id = ?", id).First(&indicator).Error
+
+  return indicator, err
+}
+
+func (imp *IndicatorMapperPSQL) Insert(indicator *Report) error {
+  return imp.db.Create(indicator).Error
+}
+
+
+func (imp *IndicatorMapperPSQL) Delete(int id) error {
+  return imp.db.Delete(&Indicator{ Id: id }).Error
+}
+
+
+func (imp *IndicatorMapperPSQL) Update(indicator *Report) error {
+  var i Indicator
+
+  imp.db.First(&i, indicator.Id)
+  i.Copy(indicator)
+
+  return imp.db.Save(indicator).Error
+}
--- app/bundles/indicatorbundle/model_indicator.go
+++ app/bundles/indicatorbundle/model_indicator.go
@@ -1,21 +1,28 @@
 package indicatorbundle
 
 type Sample struct {
-  Id    uint32  `json:"id"`
-  Date	string	`json:"date"`
-	Value	string	`json:"value"`
+  Id          int     `json:"id"`
+  IndicatorId int     `gorm:"index" json:"indicatorId"`
+  Date        string	`json:"date"`
+  Value	      string	`json:"value"`
 }
 
 type Indicator struct {
-  Id          uint32		`json:"id"`
-	Name				string		`json:"name"`
-	Description	string		`json:"description"`
-	Metric			string 		`json:"metric"`
-	Status			string 		`json:"status"`
-	Date				string 		`json:"date"`
-	Samples			[]Sample	`json:"samples"`
+  Id          int       `gorm:"AUTO_INCREMENT" json:"id"`
+  Name        string		`gorm:"not null" json:"name"`
+  Description string		`gorm:"not null" json:"description"`
+  Metric      string 		`gorm:"not null" json:"metric"`
+  Status      string 		`gorm:"not null" json:"status"`
+  Samples     []Sample	`json:"samples"`
 }
 
-func NewIndicator(id uint32, name, description, metric, status, date string, samples []Samples) *Indicator {
-  return &Indicator { Id: id, Name: name, Description: description, Metric: metric, Status: status, Date: date, Samples: samples }
+func NewIndicator(id uint32, name, description, metric, status string, samples []Samples) *Indicator {
+  return &Indicator { Id: id, Name: name, Description: description, Metric: metric, Status: status, Samples: samples }
+}
+
+func (indicator *Indicator) Copy(i *Indicator) {
+  indicator.Name = i.Name
+  indicator.Description = i.Description
+  indicator.Metric = i.Metric
+  indicator.Status = i.Status
 }
